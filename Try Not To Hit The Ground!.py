5e0d006b3f9686f21f8bfacbfadb2ae3f53c3695
LightRay785 <98335155+LightRay785@users.noreply.github.com>
2022-08-26 00:02:35 -0700

    Update Try Not To Hit The Ground!.py

--- Try Not To Hit The Ground!.py
+++ Try Not To Hit The Ground!.py
@@ -7,21 +7,31 @@ width, height = 550, 630
 screen = pygame.display.set_mode((width, height))
 fps = 240 #30 for extra easy mode 60 for easy 120 for medium 240 for hard and so on.....
 pygame.display.set_caption('Try Not To Hit The Ground!')
-flappy_bird = pygame.image.load('birdy.png')
+birdy = pygame.image.load('birdy.png')
 sky = pygame.image.load('background.png')
 ground = pygame.image.load('groundimg.jpg')
-class Bird(pygame.sprite.Sprite):
+class Player(pygame.sprite.Sprite):
+    def update(self, key): 
+        # Jump
+        self.velocity += 0.5
+        if key[pygame.K_SPACE] and self.rect.y > 0:
+            self.velocity = -7
+class Bird(Player):
     def __init__(self):
         pygame.sprite.Sprite.__init__(self)
-        self.image = flappy_bird
+        self.image = birdy
         self.rect = self.image.get_rect()
         self.rect.center = 100, 315
         self.velocity = 0
-    def update(self, key): 
+
+    def update(self, key):
         self.velocity += 0.5
+        # Restricts Player to go higher than the screen
         if self.velocity >= 6:
-            self.velocity = 7
+            self.vel = 7
         if self.rect.y < 500: self.rect.y += self.velocity
+
+        # Jump
         if key[pygame.K_SPACE] and self.rect.y > 0:
             self.velocity = -7
 
